3a3cb202107ab80ed51f71d9ca277a93cf745c59
Enzo-Riviere <104198804+Enzo-Riviere@users.noreply.github.com>
2024-10-22 20:03:16 -0400

    Update Renewable_Energy_Data_💡.py

--- Project/pages/Renewable_Energy_Data_💡.py
+++ Project/pages/Renewable_Energy_Data_💡.py
@@ -29,12 +29,12 @@ with st.sidebar:
     st.write("This is my main data visualization project. The goal of this project is to manipulate and analyze the production of renewable energy in France to understand production trends and regional differences.")
     st.write("Where you can find me :")
     
-    logo_path = os.path.join(current_dir, "images", "logo_linkedin.png")
+    logo_path = os.path.join(current_dir, "../images", "logo_linkedin.png")
     st.image(logo_path)
     url_linkedin = "www.linkedin.com/in/enzo-rivière-a55b07221"
     st.markdown("[Find me on Linkedin !](%s)"%url_linkedin)
     
-    logo_path = os.path.join(current_dir, "images", "github_logo.png")
+    logo_path = os.path.join(current_dir, "../images", "github_logo.png")
     st.image(logo_path)
     url_github = "https://github.com/Enzo-Riviere"
     st.markdown("[Find me on GitHub !](%s)"%url_github)
